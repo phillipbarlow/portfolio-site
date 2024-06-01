f3a1647d2add68299fab965e3f479b36ec273fe0
phillipbarlow10@gmail.com <phillipbarlow10@gmail.com>
2024-06-01 13:33:11 +0100

    projects page in Desktop complete

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -2,12 +2,62 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Hero from '../components/Hero';
-
+import frontendProject from '../images/frontend.png'
+import backendProject from '../images/backend-project.png'
+import leedsYouAround from '../images/leedsYouAround.png';
 export default function Work() {
   return (
     <div>
       <Header/>
-      <Hero headerName={'Work'}/>
+      <Hero headerName={'Projects'} className={'projectsClass'} headerText={'projects-text'}/>
+      <section className='all-projects-container'>
+          <img className='philsNewsFrontend-img' src={frontendProject} alt="front desktop view of phils news" />
+          <div  className='philsNewsFrontend-txt'>
+            <p className='project-title'>Phils news frontend</p>
+            <p>
+            A CRUD (create, read, update, delete) application created using React for the front-end, created during my time at Northcoders
+            </p>
+            <div className="projectBtn-container">
+            <a href="https://phils-news.netlify.app" target="_blank"> View in action</a>
+            <a href="https://github.com/phillipbarlow/nc-news" target="_blank">View on Github</a>
+            </div>
+          </div>
+          <img className='philsNewsBackend-img' src={backendProject} alt="Desktop view of my code in visual studio code" />
+          <div className='philsNewsBackend-txt'>
+            <p  className='project-title'>
+              Phils news backend
+            </p>
+            <p>
+              A RESTful API backend project built using JavaScript and Express.js to interact with a PostgreSQL database.
+            </p>
+            <div className="projectBtn-container">
+              <a href="https://nc-project-wk.onrender.com/api" target="_blank">View API</a>
+              <a href="https://github.com/phillipbarlow/nc-project-wk" target="_blank">View on Github</a>
+            </div>
+          </div>
+          <img className='LeedsYouAround-img' src={leedsYouAround} alt="" />
+          <div className='LeedsYouAround-txt'>
+            <p className='project-title'>LeedsYouAround</p>
+            <p className='LeedsYouAround-txt'>
+            My final project which I teamed up with three other northcoders to develop LeedsYouAroun from design to code within two week, the mobile scavenger hunt app focused on landmarks in the Leeds area. Players navigate to various sights using an integrated map and validate their visits by taking pictures, which are then verified and marked off. The app was built using React Native and Expo, with Firebase and Google Vision handling the backend and image recognition. We managed our project with Jira, which also inspired our team name.
+            </p>
+            <div className="projectBtn-container">
+              <a href="https://northcoders.com/project-phase/leedsyouaround" target="_blank">View in action</a>
+              <a href="https://github.com/mstent/Leeds-You-Around" target="_blank">View on Github</a>
+            </div>
+          </div>
+            <img className='portfolio-img' src={backendProject} alt="" />
+          <div className='portfolio-txt'>
+            <p className='project-title'>Portfolio site</p>
+            <p className='portfolio-txt'>
+              This site which you are viewing now, I created to demonstate some of my ability to use HTML CSS and React.
+            </p>
+            <div className="projectBtn-container">
+              <a href="https://phillipbarlow.netlify.app" target="_blank">View in action</a>
+              <a href="https://github.com/phillipbarlow/portfolio-site" target="_blank">View on Github</a>
+            </div>
+          </div>
+      </section>
       <Footer/>
     </div>
   )
